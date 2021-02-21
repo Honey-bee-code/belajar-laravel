dbea35c90837ac2369df933337a8c666b2541fb5
Honey-bee-code <hanifinida@gmail.com>
2021-02-21 08:05:10 +0800

    migrations and seeder

---
+++ database/seeds/EdulevelSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EdulevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('edulevels')->insert([
+            [
+                'name' => 'SD / Sederajat',
+                'desc' => 'SD / MI / ULA',
+            ],
+            [
+                'name' => 'SMP / Sederajat',
+                'desc' => 'SMP / MTs / Wustha',
+            ],
+        ]);
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="">Welcome</a>
+                    <a href="{{url('/')}}">Welcome</a>
+                    <a href="{{url('home')}}">Home</a>
                     <a href="">Edu Level</a>
                     <a href="">Programs</a>
                 </div>
